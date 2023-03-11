1caf5e9aaf9aedf27e1a8cff9980bf987ad435f1
fadlipurnama <fadlypurnama26@gmail.com>
2023-03-11 08:08:00 +0700

    :rocket: add ci action

--- src/pages/ProfileUserPage.jsx
+++ src/pages/ProfileUserPage.jsx
@@ -12,7 +12,7 @@ function ProfileUserPage({ authUser }) {
     dispatch(asyncReceiveLeaderboards());
     if (leaderboards !== null) {
       const filterBoard = leaderboards.filter(({ user }) => {
-        if (user.email === authUser.email) {
+        if (user?.email === authUser?.email) {
           return true;
         } else {
           return false;
@@ -33,24 +33,24 @@ function ProfileUserPage({ authUser }) {
         <h1 className='text-center text-xl font-bold md:text-2xl lg:text-3xl'>Profile Pengguna</h1>
         <div className='flex flex-col lg:my-10 lg:flex-row lg:items-center lg:justify-center lg:gap-1'>
           <div className='m-auto my-10 h-32 w-32 lg:m-0 lg:h-72 lg:w-72'>
-            <img src={authUser.avatar} alt={authUser.name} className='h-full w-full rounded-lg' />
+            <img src={authUser?.avatar} alt={authUser.name} className='h-full w-full rounded-lg' />
           </div>
           <div className='flex flex-col gap-2 px-8'>
             <div className='border-b border-slate-600 p-2 text-sm lg:border-none lg:p-1 lg:text-lg'>
               <h2 className='text-base font-semibold lg:text-xl'>Nama:</h2>
-              <p>{authUser.name}</p>
+              <p>{authUser?.name}</p>
             </div>
             <div className='border-b border-slate-600 p-2 text-sm lg:border-none lg:p-1 lg:text-lg'>
               <h2 className='text-base font-semibold lg:text-xl'>ID:</h2>
-              <p>{authUser.id}</p>
+              <p>{authUser?.id}</p>
             </div>
             <div className='border-b border-slate-600 p-2 text-sm lg:border-none lg:p-1 lg:text-lg'>
               <h2 className='text-base font-semibold lg:text-xl'>Email:</h2>
-              <p>{authUser.email}</p>
+              <p>{authUser?.email}</p>
             </div>
             <div className='border-b border-slate-600 p-2 text-sm lg:border-none lg:p-1 lg:text-lg'>
               <h2 className='text-base font-semibold lg:text-xl'>Skor yang di dapat:</h2>
-              <p>{!profileUser ? 0 : profileUser[0].score}</p>
+              <p>{!profileUser ? 0 : profileUser[0]?.score}</p>
             </div>
           </div>
         </div>
